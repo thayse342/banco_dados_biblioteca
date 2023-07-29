cf3ed09937b88b7362f5f0137c85781835b8fb66
Thayse342 <thayse342@gmail.com>
2023-07-29 20:08:50 -0300

    Correção ordem id das tabelas

--- scripts/populando_biblioteca.sql
+++ scripts/populando_biblioteca.sql
@@ -5,7 +5,7 @@ INSERT INTO Enderecos (id,rua,numero_casa,cep,complemento) VALUES
 (2,'Rua Grito de Alerta',50,956800,'Casa dos Fundos'),
 (3,'Rua Deus te Guarde',777,506678,'Segunda Casa'),
 (4,'Rua Deus te Elimine',666,34567890,'Ao lado do Hospital'),
-(5,'Rua Tenha uma Ultima Semana',22,234567800,'Cem anos Plantando Homens na Terra- Funerária Agricola');
+(5,'Rua Tenha uma Ultima Semana',22,234567800,'Cem anos');
 
 INSERT INTO Editoras (id,nome,cnpj,telefone,email) VALUES
 (6,'Construindo Sonho','956780001524','55418985067','construindosonho@gmail.com'),
@@ -29,18 +29,16 @@ INSERT INTO Pessoas (id,nome,email,cpf,telefone,nasc,id_enderecos_fk) VALUES
 (20,'Jujuba Navida','navida@email.com','789555667','991505698','2022-06-11',5);
 
 INSERT INTO Usuarios(id,senha,id_pessoas_fk) VALUES
-(20,'Adivinha',16);
-
-INSERT INTO Usuarios(id,senha,id_pessoas_fk) VALUES
-(21,'TentaDenovo',17),
-(22,'Faltoualgo',18),
-(23,'Naoerroursrs',19),
-(24,'Serioquerrou',20);
+(21,'Adivinha',16),
+(22,'TentaDenovo',17),
+(23,'Faltoualgo',18),
+(24,'Naoerroursrs',19),
+(25,'Serioquerrou',20);
 
 INSERT INTO Funcionarios(id,senha,cargo,id_pessoas_fk) VALUES
-(25,'Viverarte','Atendente',16),
-(26,'Felicidade','Bibliotecario',17),
-(27,'Sebo123','Restaurador',18),
+(26,'Viverarte','Atendente',16),
+(27,'Felicidade','Bibliotecario',17),
+(28,'Sebo123','Restaurador',18),
 (29,'Dia...','Auxiliar de Limpeza',19),
 (30,'@#isso','Bibliotecario',20);
 
@@ -60,9 +58,9 @@ INSERT INTO Livros (id,titulo, data_publicacao, qtd_paginas, edicao, isbn, idiom
 (40,'Rapunzel careca','1990-02-01',200,'Primeira',100,'BR',1,35,15,10);
 
 INSERT INTO Emprestimos (id,data_emprest,data_devolucao,id_livros_fk,id_funcionarios_fk,id_usuarios_fk) VALUES
-(41,'2021-04-13','2021-06-13',36,25,20),
-(42,'2022-05-18','2022-07-18',37,26,21),
-(43,'2023-07-20','2023-09-20',38,27,22),
-(44,'2012-08-12','2012-10-12',39,28,23),
-(45,'2000-09-13','2000-11-13',40,29,24);
+(41,'2021-04-13','2021-06-13',36,26,21),
+(42,'2022-05-18','2022-07-18',37,27,22),
+(43,'2023-07-20','2023-09-20',38,28,23),
+(44,'2012-08-12','2012-10-12',39,29,24),
+(45,'2000-09-13','2000-11-13',40,30,25);
 
